7741f90fb5ad61f9b47c3a81c10e0da62019c795
AugustoKlaic <gutosopelsa@hotmail.com>
2024-11-21 22:39:46 -0300

    feat(learningGo): added delete and update endpoints

--- pkg/api/learning_controller.go
+++ pkg/api/learning_controller.go
@@ -9,6 +9,7 @@ import (
 /*
  - Here will be created all routes for the specific domain "learning"
  - The only function exported here will be the one with the router
+ - The for loop iterates over copies of the original value <- pay attention to that
 */
 
 func getAllMessages(c *gin.Context) {
@@ -38,8 +39,45 @@ func createMessage(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newMessage)
 }
 
+func deleteMessage(c *gin.Context) {
+	var id, _ = strconv.Atoi(c.Param("id"))
+
+	for i, a := range Messages {
+		if a.Id == id {
+			Messages = append(Messages[:i], Messages[i+1:]...)
+			c.Status(http.StatusNoContent)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Message not found"})
+}
+
+func updateMessage(c *gin.Context) {
+	var id, _ = strconv.Atoi(c.Param("id"))
+
+	for i, a := range Messages {
+		if a.Id == id {
+			var updateMessage Message
+
+			if err := c.BindJSON(&updateMessage); err != nil {
+				return
+			}
+
+			Messages[i].Content = updateMessage.Content
+			Messages[i].DateTime = updateMessage.DateTime
+
+			c.IndentedJSON(http.StatusOK, Messages[i])
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Message not found"})
+}
+
 func StartLearningApiRouter(router *gin.Engine) {
 	router.GET("/learning", getAllMessages)
 	router.GET("/learning/:id", getMessage)
 	router.POST("/learning", createMessage)
+	router.DELETE("/learning/:id", deleteMessage)
+	router.PATCH("/learning/:id", updateMessage)
 }
