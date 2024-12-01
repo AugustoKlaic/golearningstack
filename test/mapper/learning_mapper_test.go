b6fee7ac97470119ce2f80e30a78ba35508100c1
AugustoKlaic <gutosopelsa@hotmail.com>
2024-11-30 23:42:49 -0300

    feat(learningGo): added mapper unit tests

--- test/mapper/learning_mapper_test.go
+++ test/mapper/learning_mapper_test.go
@@ -1,11 +1,28 @@
 package mapper
 
 import (
+	"github.com/AugustoKlaic/golearningstack/pkg/api/request"
 	"github.com/AugustoKlaic/golearningstack/pkg/domain/entity"
 	"github.com/AugustoKlaic/golearningstack/pkg/mapper"
 	"testing"
 	"time"
 )
+
+func TestMapToEntity(t *testing.T) {
+	var messageRequest = request.MessageRequest{
+		Content:  "Hello World!",
+		DateTime: time.Date(2024, 12, 1, 18, 0, 0, 0, time.UTC),
+	}
+
+	var want = mapper.ToMessageEntity(messageRequest)
+
+	if want.Content != messageRequest.Content {
+		t.Errorf("expected Content: %v, got: %v", messageRequest.Content, want.Content)
+	}
+	if want.DateTime != messageRequest.DateTime {
+		t.Errorf("expected DateTime: %v, got: %v", messageRequest.DateTime, want.DateTime)
+	}
+}
 
 func TestMapToResponse(t *testing.T) {
 	var messageEntity = entity.MessageEntity{
@@ -20,5 +37,29 @@ func TestMapToResponse(t *testing.T) {
 	}
 	if want.DateTime != messageEntity.DateTime {
 		t.Errorf("expected DateTime: %v, got: %v", messageEntity.DateTime, want.DateTime)
+	}
+}
+
+func TestMapToResponses(t *testing.T) {
+	var messageEntities = []entity.MessageEntity{
+		{
+			Content:  "Hello World!",
+			DateTime: time.Date(2024, 12, 1, 18, 0, 0, 0, time.UTC),
+		},
+		{
+			Content:  "Hello World 2!",
+			DateTime: time.Date(2024, 10, 12, 4, 0, 0, 0, time.UTC),
+		},
+	}
+
+	var want = mapper.ToMessageResponses(messageEntities...)
+
+	for i := 0; i < len(messageEntities); i++ {
+		if want[i].Content != messageEntities[i].Content {
+			t.Errorf("expected Content: %v, got: %v", messageEntities[i].Content, want[i].Content)
+		}
+		if want[i].DateTime != messageEntities[i].DateTime {
+			t.Errorf("expected DateTime: %v, got: %v", messageEntities[i].DateTime, want[i].DateTime)
+		}
 	}
 }
