8f06a50d444a5e235f429169181b1faf932a2480
AugustoKlaic <gutosopelsa@hotmail.com>
2024-12-04 00:10:54 -0300

    feat(golearningstack): done unit test to GetAllMessages

--- test/service/learning_service_test.go
+++ test/service/learning_service_test.go
@@ -1,11 +1,25 @@
 package service
 
 import (
+	"errors"
+	"github.com/AugustoKlaic/golearningstack/pkg/domain/entity"
 	"github.com/AugustoKlaic/golearningstack/pkg/service"
 	mock "github.com/AugustoKlaic/golearningstack/test/mock"
+	"github.com/go-playground/assert/v2"
 	"github.com/golang/mock/gomock"
 	"testing"
+	"time"
 )
+
+/*
+ - In unit testing when I need to mock I am using mockgen
+ - setup to use mockgen:
+ - run: go get -u github.com/golang/mock/mockgen
+ - run:go install github.com/golang/mock/mockgen@latest
+ - add this to the file that the mock will be generated:
+		//go:generate mockgen -source=service.go -destination=mock/service.go
+ - run: go generate -v ./... to generate mocks
+*/
 
 type TestSuite struct {
 	mockController  *gomock.Controller
@@ -25,16 +39,49 @@ func setupTestSuite(t *testing.T) *TestSuite {
 	}
 }
 
-func TestGetAllMessages(t *testing.T) {
-	// setup test here
+var firstMessage, secondMessage = entity.MessageEntity{
+	Id:       1,
+	Content:  "Test message 1",
+	DateTime: time.Now(),
+}, entity.MessageEntity{
+	Id:       2,
+	Content:  "Test message 1",
+	DateTime: time.Now(),
 }
 
-// link for study: https://medium.com/@ninucium/mocking-integration-tests-in-go-nina-pakshina-d01eefe5251d
-/*
-setup para usar o mockgen:
- - run: go get -u github.com/golang/mock/mockgen
- - run:go install github.com/golang/mock/mockgen@latest
- - add this to the file that the mock will be generated:
-		//go:generate mockgen -source=service.go -destination=mock/service.go
- - run: go generate -v ./... to generate mocks
-*/
+var allMessages = []entity.MessageEntity{firstMessage, secondMessage}
+
+func TestGetAllMessages(t *testing.T) {
+	var suite = setupTestSuite(t)
+
+	suite.mockRepo.EXPECT().FindAllMessages().Return(allMessages, nil).Times(1)
+
+	var expected, err = suite.learningService.GetAllMessages()
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, len(expected), 2)
+	assert.Equal(t, expected[0].Id, firstMessage.Id)
+	assert.Equal(t, expected[1].Id, secondMessage.Id)
+}
+
+func TestGetAllMessagesEmpty(t *testing.T) {
+	var suite = setupTestSuite(t)
+
+	suite.mockRepo.EXPECT().FindAllMessages().Return([]entity.MessageEntity{}, nil).Times(1)
+
+	var expected, err = suite.learningService.GetAllMessages()
+
+	assert.Equal(t, nil, err)
+	assert.Equal(t, len(expected), 0)
+}
+
+func TestGetAllMessagesError(t *testing.T) {
+	var suite = setupTestSuite(t)
+
+	suite.mockRepo.EXPECT().FindAllMessages().Return(nil, errors.New("")).Times(1)
+
+	var expected, err = suite.learningService.GetAllMessages()
+
+	assert.Equal(t, nil, expected)
+	assert.Equal(t, "problem retrieving messages: ", err.Error())
+}
